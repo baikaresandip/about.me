f9cec03a1c5fde8e2a0a340337945892344d9a47
Sandip Baikare <sandip.baikare@frontdoorhome.com>
2021-12-08 21:51:22 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,117 @@
+{
+  "basics": {
+    "name": "Sandip Baikare",
+    "label": "Sr. Software Developer",
+    "image": "https://avatars.githubusercontent.com/u/15994201",
+    "email": "baikare.sandeep007@gmail.com",
+    "phone": "+91-9860046450",
+    "url": "http://johndoe.com",
+    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "location": {
+      "address": "At Post Satala, Ta Ahmedput Dist Latur",
+      "postalCode": "413523",
+      "city": "Latur",
+      "countryCode": "In",
+      "region": "Maharashtra"
+    },
+    "profiles": [{
+      "network": "Twitter",
+      "username": "baikaresandeep",
+      "url": "http://twitter.com/BaikareSandeep"
+    }]
+  },
+  "work": [{
+    "name": "Xoriant Solutions, Pune",
+    "position": "Sr. Software Engineer",
+    "url": "http://somecompany.com",
+    "startDate": "2020-05-04",
+    "endDate": "2020-12-27",
+    "summary": "",
+    "highlights": [
+      "Lead Developer of Innovation hub"
+    ]
+  }],
+  "volunteer": [{
+    "organization": "Organization",
+    "position": "Volunteer",
+    "url": "http://organization.com/",
+    "startDate": "2020-01-01",
+    "endDate": "2020-01-01",
+    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "highlights": [
+      "Awarded 'Volunteer of the Month'"
+    ]
+  }],
+  "education": [{
+    "institution": "Savitribai Phule Pune University",
+    "url": "https://institution.ph",
+    "area": "Software Development",
+    "studyType": "Master of Compuer Application",
+    "startDate": "2011-06-15",
+    "endDate": "2014-06-10",
+    "score": "65",
+    "courses": [
+      "WEB101 - Web Development",
+      "SYS123 - Object Oriented Programming"
+    ]
+  }],
+  "awards": [{
+    "title": "Award",
+    "date": "2020-11-01",
+    "awarder": "Company",
+    "summary": "Programmer of the Year 2020"
+  }],
+  "publications": [{
+    "name": "Publication",
+    "publisher": "Company",
+    "releaseDate": "2020-10-01",
+    "url": "http://publication.com",
+    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  }],
+  "skills": [{
+    "name": "Web Development",
+    "level": "Master",
+    "keywords": [
+      "HTML",
+      "CSS",
+      "Javascript"
+    ]
+  }],
+  "languages": [{
+    "language": "English",
+    "fluency": "Native speaker"
+  }],
+  "interests": [{
+    "name": "Electronics",
+    "keywords": [
+      "PCB",
+      "IOT"
+    ],
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  }],
+  "references": [{
+    "name": "Jane Doe",
+    "reference": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  }],
+  "projects": [
+    {
+      "name": "Project Name",
+      "description": "Description...",
+      "highlights": [ 
+        "COVID19 Patient Tracker"
+      ],
+      "keywords": [
+        "PHP", "NodeJS", "Javascript"
+      ],
+      "startDate": "2020-01-01",
+      "endDate": "2020-01-01",
+      "url": "https://project.example.com",
+      "roles": [ 
+        "Lead Developer", "Designer"
+      ],
+      "image": "https://avatars.githubusercontent.com/u/15994201",
+      "entity": "Facebook, Google, Microsoft, Amazon",
+      "type": "web application"
+    }
+  ]
+}
